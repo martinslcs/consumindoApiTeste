d5ac82c1f0b031097914f9237d15ee89faf6b586
matinslcs <ccplucasmartins@gmail.com>
2025-01-27 22:45:07 -0300

    primeiro

---
+++ ConsumindoApi/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsumindoApi;
+using System.Net.Http.Json;
+
+var client = new HttpClient();
+
+
+var response = await client.GetAsync("https://jsonplaceholder.typicode.com/posts");
+
+var posts = await response.Content.ReadFromJsonAsync<List<Post>>();
+
+if (response.IsSuccessStatusCode)
+{
+    Console.WriteLine("Sucesso!");
+}
+else
+{
+    Console.WriteLine($"Deu ruim: {response.StatusCode}");
+}
+
+for (int i = 0; i < 5; i++)  
+{
+    Console.WriteLine($"Id:{posts[i].Id} - Title: {posts[i].Title} - Body: {posts[i].Body}");
+}
+
+Console.ReadLine();
